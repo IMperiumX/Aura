58e499cff4934dd9508d339cf173a666aa6c9871
IMperiumX <yusufadell.dev@gmail.com>
2024-07-03 07:39:05 +0300

    first commit

---
+++ aura/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from aura.users.models import User
+from aura.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
