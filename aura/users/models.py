8c01822e74c10b329a4c836384f4bcef27de3ead
IMperiumX <yusufadell.dev@gmail.com>
2024-07-05 09:20:15 +0300

    chore: Update User model to use Django's models module for fields

--- aura/users/models.py
+++ aura/users/models.py
@@ -1,9 +1,7 @@
-
 from typing import ClassVar
 
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-from django.db.models import EmailField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -18,10 +16,10 @@ class User(AbstractUser):
     """
 
     # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
-    email = EmailField(_("email address"), unique=True)
+    email = models.EmailField(_("email address"), unique=True)
     username = None  # type: ignore[assignment]
 
     USERNAME_FIELD = "email"
@@ -37,3 +35,50 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"pk": self.id})
+
+
+class UserProfile(models.Model):
+    """
+    A model to represent a user's profile.
+    """
+
+    class GenderType(models.TextChoices):
+        """
+        Choices for the type of user gender.
+        """
+
+        MALE = "m", _(" Male")
+        FEMALE = "f", _("Female")
+
+    bio = models.TextField(
+        verbose_name="Bio",
+    )
+    date_of_birth = models.DateField(
+        verbose_name="Date of Birth",
+    )
+    health_data = models.JSONField(null=True, blank=True)
+    preferences = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Created At",
+    )
+    weight = models.FloatField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    gender = models.CharField(
+        max_length=1,
+        choices=GenderType.choices,
+    )
+    user = models.OneToOneField(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="profile",
+        verbose_name="User",
+    )
+
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name = "User Profile"
+        verbose_name_plural = "User Profiles"
+
+    def __str__(self):
+        return f"{self.user} - {self.created_at}"
