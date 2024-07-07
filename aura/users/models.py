042e05dc9901c49ad77d390f339da4ddea4c464d
Restyled.io <commits@restyled.io>
2024-07-07 02:08:12 +0000

    Restyled by pyment

--- aura/users/models.py
+++ aura/users/models.py
@@ -14,7 +14,7 @@ from .managers import UserManager
 def sane_repr(*attrs: str) -> Callable[[object], str]:
     """
 
-    :param *attrs: str:
+    :param *attrs: str: 
 
     """
     if "id" not in attrs and "pk" not in attrs:
@@ -26,6 +26,7 @@ def sane_repr(*attrs: str) -> Callable[[object], str]:
         :param self: object:
         :param self: object:
         :param self: object:
+        :param self: object: 
 
         """
         cls = type(self).__name__
@@ -97,7 +98,7 @@ class User(AbstractUser):
     def set_password(self, raw_password):
         """
 
-        :param raw_password:
+        :param raw_password: 
 
         """
         super().set_password(raw_password)
