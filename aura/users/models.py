76ecbe39d477a3744a5ced934fb14c2803b77c17
Restyled.io <commits@restyled.io>
2024-07-07 02:07:28 +0000

    Restyled by reorder-python-imports

--- aura/users/models.py
+++ aura/users/models.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
-from typing import Any, ClassVar
+from typing import Any
+from typing import ClassVar
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
