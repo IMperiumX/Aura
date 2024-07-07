1a4d51122cc5c14d63ebfa45fc82ebe8bdaf30f6
IMperiumX <yusufadell.dev@gmail.com>
2024-07-07 19:22:15 +0300

    chore: Update admin.py with HealthAssessment and HealthRiskPrediction models in aura/assessments

--- aura/assessments/admin.py
+++ aura/assessments/admin.py
@@ -1,1 +1,38 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import HealthAssessment
+from .models import HealthRiskPrediction
+
+
+@admin.register(HealthAssessment)
+class HealthAssessmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "created",
+        "modified",
+        "status",
+        "status_changed",
+        "assessment_type",
+        "risk_level",
+        "recommendations",
+        "responses",
+        "result",
+        "patient",
+    )
+    list_filter = ("created", "modified", "status_changed", "patient")
+
+
+@admin.register(HealthRiskPrediction)
+class HealthRiskPredictionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "created",
+        "modified",
+        "health_issue",
+        "preventive_measures",
+        "confidence_level",
+        "source",
+        "assessment",
+        "patient",
+    )
+    list_filter = ("created", "modified", "assessment", "patient")
