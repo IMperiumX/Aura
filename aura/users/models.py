ccf8a8d265375126702d149a1375343fd08eca0d
Restyled.io <commits@restyled.io>
2024-07-07 02:05:34 +0000

    Restyled by whitespace

--- aura/users/models.py
+++ aura/users/models.py
@@ -15,7 +15,7 @@ from .managers import UserManager
 def sane_repr(*attrs: str) -> Callable[[object], str]:
     """
 
-    :param *attrs: str: 
+    :param *attrs: str:
 
     """
     if "id" not in attrs and "pk" not in attrs:
@@ -25,7 +25,7 @@ def sane_repr(*attrs: str) -> Callable[[object], str]:
         """
 
         :param self: object:
-        :param self: object: 
+        :param self: object:
 
         """
         cls = type(self).__name__
@@ -97,7 +97,7 @@ class User(AbstractUser):
     def set_password(self, raw_password):
         """
 
-        :param raw_password: 
+        :param raw_password:
 
         """
         super().set_password(raw_password)
