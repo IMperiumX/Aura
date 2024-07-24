9ad21dcfe4302608c66a132cefa35916fa1999b2
IMperiumX <yusufadell.dev@gmail.com>
2024-07-25 01:35:24 +0300

    chore: Update SERVE_PERMISSIONS to allow any user to access API endpoints

--- config/settings/base.py
+++ config/settings/base.py
@@ -348,7 +348,7 @@ SPECTACULAR_SETTINGS = {
     "TITLE": "aura API",
     "DESCRIPTION": "Documentation of API endpoints of aura",
     "VERSION": "1.0.0",
-    "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
+    "SERVE_PERMISSIONS": ["rest_framework.permissions.AllowAny"],
     "SCHEMA_PATH_PREFIX": "/api/",
 }
 # Your stuff...
