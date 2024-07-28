a06900645033281a7635d0f90c912c684362a07e
IMperiumX <yusufadell.dev@gmail.com>
2024-07-29 02:56:03 +0300

    Building a RAG pipeline
    - get data into an LLM, and a component of more sophisticated agentic systems.
    
    Loading & Ingestion
    
    Indexing and Embedding
    
    Storing in a specialized database known as a Vector Store
    
    Querying: Every indexing strategy has a corresponding querying strategy, LLM improve the relevance, speed and accuracy of what you retrieve before returning it to you.
    
    Turning it into structured responses such as an API.

--- aura/core/services.py
+++ aura/core/services.py
@@ -4,20 +4,33 @@
     - [x] Patient profiles and related models.
     - [x] vector similarity search for Postgres
     - [x] Custom Querysets
-    - [ ] machine learning models.
+    - [x] machine learning models.
+    - [x] Building a RAG pipeline
 - Incorporating user feedback on recommendations to improve future suggestions.
 """
 
-# import re
+import re
 
-# from llama_index import Document, download_loader
-# from llama_index.embeddings import HuggingFaceEmbedding
+from django.conf import settings
+from llama_index.core import Document
+from llama_index.core import Settings
+from llama_index.core import StorageContext
+from llama_index.core import VectorStoreIndex
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.readers.database import DatabaseReader
+from llama_index.vector_stores.postgres import PGVectorStore
+from rest_framework.response import Response
 
-# You would cache this
-# EMBED_MODEL = HuggingFaceEmbedding(
-#     model_name="WhereIsAI/UAE-Large-V1",
-#     embed_batch_size=10,  # open-source embedding model
-# )
+# open-source
+from transformers import AutoTokenizer
+
+# change chunk size and overlap without changing the default splitter
+Settings.chunk_size = 512
+Settings.chunk_overlap = 20
+
+CONTEXT_WINDOW = 3900
+DATABASE = settings.DATABASES["default"]
 
 
 class RecommendationEngine:
@@ -33,183 +46,115 @@ class RecommendationEngine:
     def find_best_match(self, health_assessment):
         return self.get_therapist_recommendations(health_assessment).first()
 
-    # @classmethod
-    # def get_mental_health_recommendations(cls, health_assessment):
-    #     from aura.assessments.models import HealthAssessment, HealthRiskPrediction
+    def fetch_documents_from_storage(self, query: str) -> list[Document]:
+        reader = DatabaseReader(
+            scheme="postgresql",
+            host=DATABASE["HOST"],
+            port=DATABASE["PORT"],
+            user=DATABASE["USER"],
+            password=DATABASE["PASSWORD"],
+            dbname=DATABASE["NAME"],
+        )
+        return reader.load_data(query=query)
 
-    #     if (
-    #         health_assessment.assessment_type
-    #         != HealthAssessment.AssessmentType.MENTAL_HEALTH
-    #     ):
-    #         return []
+    def save_embeddings(self, documents: list[Document]) -> None:
+        # TODO: post_save for intentded models
+        from assessments.models import HealthAssessment
 
-    #     risk_level = health_assessment.risk_level
-    #     responses = health_assessment.responses
+        query = str(
+            HealthAssessment.objects.only(
+                "responses",
+                "result",
+                "recommendations",
+            ).query,
+        )
+        documents = self.fetch_documents_from_storage(query=query)
+        for document in documents:
+            assessment_id = re.search(r"\d+", document.text).group(0)
 
-    #     # Get base recommendations based on risk level
-    #     recommendations = HealthRiskPrediction.objects.filter(risk_level=risk_level)
+            assessment = HealthAssessment.objects.get(pk=assessment_id)
 
-    #     # Personalize recommendations based on assessment responses
-    #     return [rec for rec in recommendations if cls._is_relevant(rec, responses)]
+            embedding = Settings.embed_model.get_text_embedding(document.text)
+            assessment.embedding = embedding
 
-    # @staticmethod
-    # def _is_relevant(recommendation, responses):
-    #     # Implement logic to determine if a recommendation is relevant
-    #     # based on the user's responses
-    #     # This is a simplified example and
-    #     return (
-    #         "anxiety" in responses
-    #         and "anxiety" in recommendation.category.lower()
-    #         or "depression" in responses
-    #         and "depression" in recommendation.category.lower()
-    #     )
+            assessment.save()
 
-    # @staticmethod
-    # def find_best_match(assessment):
-    #     # TODO: Refactor with llama_index.HuggingFaceEmbedding
-    #     import numpy as np
-    #     from sklearn.metrics.pairwise import cosine_similarity
+    def setup_pgvector_store(self):
+        return PGVectorStore.from_params(
+            database=DATABASE["NAME"],
+            host=DATABASE["HOST"],
+            password=DATABASE["PASSWORD"],
+            port=DATABASE["PORT"],
+            user=DATABASE["USER"],
+            table_name="assessment_embeddings",  # XXX: data_{table_name}
+            embed_dim=settings.EMBEDDING_MODEL_DIMENSIONS,  # Must match your embedding model
+        )
 
-    #     from aura.users.models import Therapist
+    def _provide_context(self):
+        from llama_index.llms.llama_cpp import LlamaCPP
+        from llama_index.llms.llama_cpp.llama_utils import completion_to_prompt
+        from llama_index.llms.llama_cpp.llama_utils import messages_to_prompt
 
-    #     therapists = Therapist.objects.all()
-    #     assessment_embedding = np.array(assessment.embedding).reshape(1, -1)
+        Settings.text_splitter = SentenceSplitter(chunk_size=1024)
 
-    #     best_match = None
-    #     highest_similarity = -1
+        llm = LlamaCPP(
+            model_path=settings.LLAMA_GGUFF_MODEL_PATH,
+            context_window=CONTEXT_WINDOW,  # Tune to your model - Mixtral 8x7b is a beast.
+            max_new_tokens=256,
+            # set to at least 1 to use GPU
+            model_kwargs={"n_gpu_layers": settings.USE_GPU},
+            verbose=True,
+            # transform inputs into Llama2 format
+            messages_to_prompt=messages_to_prompt,
+            completion_to_prompt=completion_to_prompt,
+        )
+        Settings.llm = llm
 
-    #     for therapist in therapists:
-    #         therapist_embedding = np.array(therapist.embedding).reshape(1, -1)
-    #         similarity = cosine_similarity(assessment_embedding, therapist_embedding)[
-    #             0
-    #         ][0]
+        embed_model = HuggingFaceEmbedding(
+            model_name=settings.EMBED_MODEL_NAME,
+            embed_batch_size=10,
+            cache_dir=settings.LLAMA_INDEX_CACHE_DIR,
+        )
+        Settings.embed_model = embed_model
+        # compatible with the open-source LLM.
+        Settings.tokenzier = AutoTokenizer.from_pretrained(
+            settings.TOKENIZER_NAME,
+        )
 
-    #         if similarity > highest_similarity:
-    #             highest_similarity = similarity
-    #             best_match = therapist
+        Settings.context_window = CONTEXT_WINDOW
 
-    #     return best_match
+    def generate_vector_store_index(
+        self,
+        vector_store: PGVectorStore,
+        documents: list[Document],
+    ) -> VectorStoreIndex:
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        return VectorStoreIndex.from_documents(
+            documents,
+            storage_context=storage_context,
+            embed_model=Settings.embed_model,
+            show_progress=True,
+        )
 
-    # def fetch_documents_from_storage(self, query: str) -> list[Document]:
-    #     # Prep documents - fetch from DB
-    #     from django.conf import settings
-    #     db_url = settings.DATABASES["default"]
+    def recommend_therapist(self, k=3):
+        from assessments.models import HealthAssessment
 
-    #     DatabaseReader = download_loader("DatabaseReader")
-    #     reader = DatabaseReader(
-    #         scheme="postgresql",  # Database Scheme
-    #         host=db_url["HOST"],  # Database Host
-    #         port=db_url["PORT"],  # Database Port
-    #         user=db_url["USER"],  # Database User
-    #         password=db_url["PASSWORD"],  # Database Password
-    #         dbname=db_url["NAME"],  # Database Name
-    #     )
-    #     return reader.load_data(query=query)
+        fields = (
+            "responses",
+            "result",
+            "recommendations",
+        )
+        query = str(HealthAssessment.objects.only(*fields).query)
+        documents = self.fetch_documents_from_storage(query)
 
-    # def save_embeddings(self, documents: list[Document]) -> None:
-    #     query = f"""
-    #     SELECT e.episode_number, e.title, t.content
-    #     FROM transcriber_episode e
-    #     LEFT JOIN transcriber_transcript t ON e.id = t.episode_id;
-    #     """
-    #     documents = self.fetch_documents_from_storage(query=query)
-    #     for document in documents:
-    #         match = re.match(r"^(\d{1,3})", document.text)
-    #         if match:
-    #             episode_number = int(
-    #                 match.group(1)
-    #             )  # Convert to int for exact matching
+        vector_store = self.setup_pgvector_store()
+        index = self.generate_vector_store_index(
+            documents=documents,
+            vector_store=vector_store,
+        )
 
-    #         # Fetch the episode using get() for a single object
-    #         # episode = Episode.objects.get(episode_number=episode_number)
-
-    #         # transcript = classmethod.objects.get(episode=episode)
-
-    #         # # Generate and save the embedding
-    #         # embedding = EMBED_MODEL.get_text_embedding(document.text)
-    #         # transcript.embedding = embedding
-
-    #         # #XXX: We will make further use of these embeddings next.
-    #         # transcript.save()
-
-    # def setup_pgvector_store(self):
-    #     from urllib.parse import urlparse
-
-    #     from llama_index.vector_stores import PGVectorStore
-
-    #     from django.conf import settings
-
-    #     # settings are omitted for brevity, but a Postgres URL looks like this:
-    #     # postgresql://postgres:ff@localhost:5432/aura
-
-    #     db_url = settings.DATABASES["default"]
-    #     parsed_url = urlparse(db_url)
-    #     vector_store = PGVectorStore.from_params(
-    #         database=db_configs["NAME"],
-    #         host=db_configs["HOST"],
-    #         password=db_configs["PASSWORD"],
-    #         port=db_configs["PORT"],
-    #         user=db_configs["USER"],
-    #         table_name="assessment_embeddings", #XXX: Update if needed prefixed with `data_` => (data_podcast_embeddings)
-    #         embed_dim=settings.EMBEDDING_MODEL_DIMENSIONS,  # Must match your embedding model
-    #     )
-
-    # def _provide_context(self):
-    #     from llama_index.llms import LlamaCPP
-    #     from llama_index.embeddings import HuggingFaceEmbedding
-    #     from llama_index import set_global_tokenizer, ServiceContext
-    #     from transformers import AutoTokenizer
-
-    #     llm = LlamaCPP(
-    #                 model_path="path/to/your/GGUF/model",  # Download the GGUF from hugging face
-    #                 context_window=30000,  # Tune to your model - Mixtral 8x7b is a beast.
-    #                 max_new_tokens=1024,
-    #                 model_kwargs={"n_gpu_layers": 1},  # >=1 means using GPU. 0 means using CPU.
-    #                 verbose=True,
-    #             )
-
-    #     embed_model = HuggingFaceEmbedding(
-    #         model_name="WhereIsAI/UAE-Large-V1", embed_batch_size=10  # open-source embedding model
-    #     )
-
-    #     # set_global_tokenizer to be compatible with the open-source LLM.
-    #     set_global_tokenizer(
-    #         AutoTokenizer.from_pretrained(f"mistralai/Mixtral-8x7B-Instruct-v0.1").encode)  # must match your LLM
-
-    #     service_context = ServiceContext.from_defaults(
-    #         llm=llm,
-    #         embed_model=embed_model,
-    #         system_prompt="You are a bot that answers questions in English.",
-    #     )
-
-    # def recommend_therapist(self):
-    #     from llama_index import (
-    #         ServiceContext,
-    #         VectorStoreIndex,
-    #         StorageContext,
-    #         Document,
-    #     )
-    #     from llama_index.vector_stores import PGVectorStore
-
-    #     def generate_vector_store_index(
-    #         vector_store: PGVectorStore,
-    #         documents: list[Document],
-    #         service_context: ServiceContext,
-    #     ) -> VectorStoreIndex:
-    #         storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    #         return VectorStoreIndex.from_documents(
-    #             documents,
-    #             storage_context=storage_context,
-    #             service_context=service_context,
-    #             show_progress=True,
-    #         )
-
-    #     index = generate_vector_store_index(
-    #         documents=documents,  # We fetched these in step 3
-    #         service_context=service_context,  # We prepped this in step 6
-    #         vector_store=vector_store,  # We prepped this in step 5
-    #     )
-    #     query_engine = index.as_query_engine(
-    #         similarity_top_k=3,
-    #     )
-    #     response = query_engine.query(f"Who is Dylan Patel?")
+        query_engine = index.as_query_engine(
+            similarity_top_k=k,
+        )
+        response = query_engine.query("Best Therapist for me?")
+        return Response(response)
