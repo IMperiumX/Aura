0e82c21e6b86da7304d9561b59d2308b67109b21
IMperiumX <yusufadell.dev@gmail.com>
2024-08-02 19:53:43 +0300

    feat: Update HealthAssessmentSerializer to include patient and health risk predictions

--- aura/assessments/api/serializers.py
+++ aura/assessments/api/serializers.py
@@ -1,17 +1,20 @@
 from rest_framework import serializers
 
-from aura.assessments.models import HealthAssessment, HealthRiskPrediction
+from aura.assessments.models import HealthAssessment
+from aura.assessments.models import HealthRiskPrediction
 from aura.users.api.serializers import PatientSerializer
-from aura.users.models import Patient
-
-from decimal import Decimal
 
 
-class HealthRiskPredictionSerializer(serializers.ModelSerializer):
+class HealthRiskPredictionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = HealthRiskPrediction
         fields = [
+            "url",
             "id",
+            "health_issue",
+            "preventive_measures",
+            "confidence_level",
+            "source",
         ]
 
 
@@ -42,8 +45,8 @@ class HealthAssessmentSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Responses must be a JSON object")
         return value
 
-    # def create(self, validated_data):
-    #     patient_id = self.context["request"].data.get("patient_id")
-    #     patient = Patient.objects.get(id=patient_id)
-    #     validated_data["patient"] = patient
-    #     return super().create(validated_data)
+
+class HealthAssessmentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HealthAssessment
+        fields = ["assessment_type", "responses"]
