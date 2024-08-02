f823b636e31701bc367acd10f44ce7c2103b2217
IMperiumX <yusufadell.dev@gmail.com>
2024-08-02 19:55:06 +0300

    feat: Refactor permissions and serializers for user-related models

--- aura/users/api/permissions.py
+++ aura/users/api/permissions.py
@@ -1,22 +1,28 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsOwnerOrAdmin(BasePermission):
+class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
-            return obj.patient == request.user or request.user.is_staff
-        return (
-            obj.patient == request.user
-            or request.user == obj.therapist
-            or request.user.is_staff
-        )
-
-
-class IsTherapistOrAdmin(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.therapist or request.user.is_staff
+            return obj.patient == request.user
+        return obj.patient == request.user or request.user == obj.therapist
 
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_staff
+
+
+class IsTherapist(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.therapist
+
+
+class IsPatient(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.patient
+
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
