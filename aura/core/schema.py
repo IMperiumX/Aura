bf10322207afccb8efc494360ec731dd62c6b9cf
IMperiumX <yusufadell.dev@gmail.com>
2024-08-03 16:07:32 +0300

    feat: Add RestAuthLoginView to handle login requests
    This commit adds a new class called `RestAuthLoginView` to the `aura.core.schema` module. This class extends the `OpenApiViewExtension` and is used to handle login requests. It replaces the existing `LoginView` class and includes a `post` method that is decorated with the `extend_schema` decorator. The `post` method accepts a `request` object and performs some actions. This change improves the login functionality of the application.

--- aura/core/schema.py
+++ aura/core/schema.py
@@ -1,3 +1,24 @@
+class RestAuthLoginView(OpenApiViewExtension):
+    target_class = "dj_rest_auth.views.LoginView"
+
+    def view_replacement(self):
+        class Fixed(self.target_class):
+            @extend_schema(responses=self.get_token_serializer_class())
+            def post(self, request, *args, **kwargs):
+                pass  # pragma: no cover
+
+        return Fixed
+
+    def get_token_serializer_class(self):
+        from aura.users.api.serializers import JWTSerializer, TokenSerializer
+
+        use_jwt = settings.USE_JWT
+
+        if use_jwt:
+            return JWTSerializer
+        else:
+            return TokenSerializer
+
 
 
 class SimpleJWTCookieScheme(SimpleJWTScheme):
