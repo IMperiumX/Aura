78394c2afb0acb68907a36c003f53e92808f9c49
IMperiumX <yusufadell.dev@gmail.com>
2024-08-03 16:09:09 +0300

    refactor: Update API router configuration
    This commit updates the `api_router.py` file in the `config` directory. The changes include:
    - Removing the import statement for `django.urls.path`
    - Adding a new route for the `patients` endpoint using the `PatientViewSet`
    - Updating the `basename` for the `users` and `approaches` endpoints
    
    These changes improve the configuration of the API router and ensure that the correct views are registered for the corresponding endpoints.

--- config/api_router.py
+++ config/api_router.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.urls import path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from aura.assessments.api.views import HealthAssessmentViewSet
@@ -7,14 +6,20 @@ from aura.assessments.api.views import HealthRiskPredictionViewSet
 from aura.mentalhealth.api.views import ChatbotInteractionViewSet
 from aura.mentalhealth.api.views import TherapyApproachViewSet
 from aura.mentalhealth.api.views import TherapySessionViewSet
-from aura.users.api.views import LoginView
 from aura.users.api.views import UserViewSet
+from aura.users.api.views import PatientViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register(
     "users",
     UserViewSet,
+    basename="users",
+)
+router.register(
+    "patients",
+    PatientViewSet,
+    basename="patients"
 )
 router.register(
     "assessments",
@@ -33,9 +38,9 @@ router.register(
 )
 
 router.register(
-    "therapy-approaches",
+    "approaches",
     TherapyApproachViewSet,
-    basename="therapy-approaches",
+    basename="approaches",
 )
 router.register(
     "chatbot-interactions",
@@ -45,8 +50,3 @@ router.register(
 
 app_name = "api"
 urlpatterns = router.urls
-
-
-urlpatterns.append(
-    path("login/", LoginView.as_view(), name="rest_login"),
-)
