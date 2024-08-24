7d99b5b8e75f474063c8000caf1bc673494b18f7
IMperiumX <yusufadell.dev@gmail.com>
2024-08-24 18:32:58 +0300

    refactor: Add TherapySessionThread, Attachment, and Folder models to communication app

--- aura/communication/admin.py
+++ aura/communication/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
+from .models import Attachment
+from .models import Folder
 from .models import Message
+from .models import TherapySessionThread
 from .models import Thread
 
 
@@ -33,10 +36,52 @@ class MessageAdmin(admin.ModelAdmin):
         "modified",
         "text",
         "read_at",
-        "encrypted_content",
         "message_type",
         "data_retention_period",
         "thread",
         "sender",
     )
     list_filter = ("created", "modified", "read_at", "thread", "sender")
+
+
+@admin.register(TherapySessionThread)
+class TherapySessionThreadAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "created",
+        "modified",
+        "subject",
+        "is_group",
+        "is_active",
+        "last_message",
+        "session",
+    )
+    list_filter = (
+        "created",
+        "modified",
+        "is_group",
+        "is_active",
+        "last_message",
+        "session",
+    )
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "created",
+        "modified",
+        "file",
+        "version_number",
+        "message",
+        "previous_version",
+    )
+    list_filter = ("created", "modified", "message", "previous_version")
+
+
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ("id", "created", "modified", "name", "user", "parent")
+    list_filter = ("created", "modified", "user", "parent")
+    search_fields = ("name",)
