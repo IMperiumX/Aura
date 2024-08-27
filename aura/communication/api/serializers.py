e6a2167c86fc8afd4b16b2875d6d9fe7b84c1fe2
IMperiumX <yusufadell.dev@gmail.com>
2024-08-27 07:55:32 +0300

    Update communication serializers

--- aura/communication/api/serializers.py
+++ aura/communication/api/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
+from aura.communication.models import Attachment
+from aura.communication.models import Folder
 from aura.communication.models import Message
+from aura.communication.models import TherapySessionThread
 from aura.communication.models import Thread
 
 
@@ -21,3 +24,25 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ["id", "text", "read_at", "thread", "sender", "created", "modified"]
+
+
+class AttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attachment
+        fields = ["id", "file", "message"]
+
+
+class FolderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Folder
+        fields = ["id", "name", "user", "threads"]
+
+
+class TherapySessionThreadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TherapySessionThread
+        fields = [
+            "id",
+            "session",
+            "thread",
+        ]
