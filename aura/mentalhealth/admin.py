e5db9e53932ca756eab1c533fca564f5968261ca
IMperiumX <yusufadell.dev@gmail.com>
2024-09-12 08:33:23 +0300

    Refactor TherapySession model to remove unused code and improve code organization

--- aura/mentalhealth/admin.py
+++ aura/mentalhealth/admin.py
@@ -2,15 +2,18 @@ from django.contrib import admin
 
 from .models import ChatbotInteraction
 from .models import Disorder
+from .models import TherapyApproach
 from .models import TherapySession
+
+ADMIN_CHAR_LIMNIT = 50
 
 
 @admin.register(TherapySession)
 class TherapySessionAdmin(admin.ModelAdmin):
-    """ """
-
     list_display = (
         "id",
+        "created",
+        "modified",
         "session_type",
         "status",
         "summary",
@@ -18,44 +21,64 @@ class TherapySessionAdmin(admin.ModelAdmin):
         "scheduled_at",
         "started_at",
         "ended_at",
-        "created",
+        "target_audience",
+        "recurrences",
         "therapist",
         "patient",
     )
     list_filter = (
+        "created",
+        "modified",
         "scheduled_at",
         "started_at",
         "ended_at",
-        "created",
         "therapist",
         "patient",
     )
-    date_hierarchy = "created"
+
+
+@admin.register(TherapyApproach)
+class TherapyApproachAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "limited_description", "created", "modified")
+    list_filter = ("created", "modified")
+    search_fields = ("name",)
+
+    @admin.display(
+        description="Description",
+    )
+    def limited_description(self, obj):
+        return obj.description[:ADMIN_CHAR_LIMNIT] + "..."
 
 
 @admin.register(ChatbotInteraction)
 class ChatbotInteractionAdmin(admin.ModelAdmin):
-    """ """
-
     list_display = (
         "id",
+        "created",
+        "modified",
         "message",
         "response",
         "conversation_log",
         "interaction_date",
-        "created",
         "user",
     )
-    list_filter = ("interaction_date", "created", "user")
-    date_hierarchy = "created"
+    list_filter = ("created", "modified", "interaction_date", "user")
 
 
 @admin.register(Disorder)
 class DisorderAdmin(admin.ModelAdmin):
-    """ """
-
     list_display = (
         "id",
+        "created",
+        "modified",
         "name",
+        "type",
+        "signs_and_symptoms",
         "description",
+        "treatment",
+        "symptoms",
+        "causes",
+        "prevention",
     )
+    list_filter = ("created", "modified")
+    search_fields = ("name",)
