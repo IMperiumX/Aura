386eda070561506d5ec41b5497d7065137001156
IMperiumX <yusufadell.dev@gmail.com>
2025-06-27 19:07:13 +0300

    fix(auth): Reorder authentication classes in REST framework settings
    - Adjusted the order of authentication classes in the `REST_FRAMEWORK` settings to prioritize `JWTCookieAuthentication` over `SessionAuthentication` and `TokenAuthentication`. This change ensures that JWT-based authentication is the primary method, enhancing security and consistency in user authentication processes.

--- config/settings/base.py
+++ config/settings/base.py
@@ -494,9 +494,9 @@ SOCIALACCOUNT_FORMS = {"signup": "aura.users.forms.UserSocialSignupForm"}
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
         "aura.core.authentication.JWTCookieAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
